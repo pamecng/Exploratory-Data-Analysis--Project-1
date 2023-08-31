0a9e11ef1bfa0fa6a4d1d8e37dd38f64f20bee1c
pamecng <pamela.navarrogonzales@gmail.com>
2023-08-31 15:01:09 -0400

    Plot 2

--- Plot2.R
+++ Plot2.R
@@ -1,14 +1,19 @@
-#Opening and subsetting data
+# Loading and subsetting data 
 power <- read.table("household_power_consumption.txt",skip=1,sep=";")
 names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
 
-#calling the basic plot function
-hist(as.numeric(as.character(subpower$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+# Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
+subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
+subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
+subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 
-# annotating graph
-title(main="Global Active Power")
+
+# Plot function
+plot(subpower$Time,as.numeric(as.character(subpower$Global_active_power)),type="l",xlab="",ylab="Global Active Power (kilowatts)") 
+
 
 #copying to png
-dev.copy(png, filename="plot1.png")
+dev.copy(png, filename="plot2.png")
 dev.off()
